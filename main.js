4fcfae2289489b734e8cd67447e42ba870915844
sarawuwu <sarawu0001@gmail.com>
2022-09-06 22:56:09 +0800

    Update for videos

---
+++ main.js
@@ -0,0 +1,38 @@
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '350',
+        width: '640',
+        videoId: '74icGOK7jKw',
+        playerVars: {
+            autoplay: 0,
+            controls: 1,
+            start: 30
+        },
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady() {
+    // $("#playButton").on("click", function () {
+    //     $("h2").text(player.getVideoData().title);
+    //     player.playVideo();
+    // }
+    // )
+    player.playVideo();
+    player.seekTo(65);
+    setInterval(function () {
+        var sec = Math.round(player.getCurrentTime() * 10) / 10;
+        if (sec > 555) {
+            player.seekTo(65);
+        }
+        if (sec < 65) {
+            player.seekTo(65);
+        }
+    }, 1000);
+}
+
+
